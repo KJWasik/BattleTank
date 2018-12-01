090575b4bf94627bd00df1ec09e5b8f64cecd64c
LAPTOP-OJE1NJ4K\krzys <krzysztofwasik1988@wp.pl>
2018-12-01 21:04:02 +0000

    Turret physically rotates.

--- BattleTank/Source/BattleTank/Public/TankTurret.h
+++ BattleTank/Source/BattleTank/Public/TankTurret.h
@@ -13,5 +13,9 @@ UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class BATTLETANK_API UTankTurret : public UStaticMeshComponent
 {
 	GENERATED_BODY()
-	
+public:
+	void Rotate(float RelativeSpeed);
+private:
+	UPROPERTY(EditAnywhere, Category = Setup)
+	float MaxDegreesPerSecond = 25;
 };
